1932353fb99b06cb3b4670e7855b6f19193124cb
JosephGrieco <joseph.grieco@edu.immaginazioneelavoro.it>
2025-09-30 09:26:00 +0200

    Lezione 02

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log("Hello, World!");
+
+function nero() {
+    let quadrato = document.getElementById("quadrato");
+    quadrato.style.backgroundColor = "black";
+}
+
+function blu() {
+    let quadrato = document.getElementById("quadrato");
+    quadrato.style.backgroundColor = "blue";
+}
+
+function giallo() {
+    let quadrato = document.getElementById("quadrato");
+    quadrato.style.backgroundColor = "yellow";
+}
+
+function rosa() {   
+    let quadrato = document.getElementById("quadrato");
+    quadrato.style.backgroundColor = "pink";
+}
